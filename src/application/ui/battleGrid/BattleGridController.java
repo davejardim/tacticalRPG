db7e2024e1f19ba38ef507bec1452a441a47240f
Mark Bluemer <bluemer.mark@gmail.com>
2016-04-04 20:52:16 -0400

    Consolidated battleGrid and unitGrid into BattleGrid

--- src/application/ui/battleGrid/BattleGridController.java
+++ src/application/ui/battleGrid/BattleGridController.java
@@ -1,30 +1,72 @@
 package application.ui.battleGrid;
 
-import java.io.IOException;
-
-import application.Main;
+import application.ui.ScreenControl;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.LoadException;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 
 public class BattleGridController {
 	
 	@FXML
 	private StackPane stack;
+	private GridPane environmentGrid;
+	private GridPane unitGrid;
+	private final int GRID_SIZE = 25;
 	
 	public void initialize() {
-		try {
-			
-			GridPane environmentGrid = FXMLLoader.load(Main.class.getResource("/application/ui/environmentGrid/EnvironmentGrid.fxml"));
-			GridPane unitGrid = FXMLLoader.load(Main.class.getResource("/application/ui/unitGrid/UnitGrid.fxml"));
-			stack.getChildren().addAll(environmentGrid, unitGrid);
-		} catch (LoadException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		} catch (IOException e) {
-			e.printStackTrace();
+		buildEnvironmentGrid();
+		buildUnitGrid();
+		stack.getChildren().addAll(environmentGrid, unitGrid);
+	}
+	
+	private void buildEnvironmentGrid() {
+		environmentGrid = new GridPane();
+		environmentGrid.setGridLinesVisible(true);
+		// Not sure what these do right now -_-
+		// https://stackoverflow.com/questions/31095954/how-to-get-gridpane-row-and-column-ids-on-mouse-entered-in-each-cell-of-grid-in
+		for (int i = 0 ; i < GRID_SIZE ; i++) {
+	        ColumnConstraints colConstraints = new ColumnConstraints();
+	        colConstraints.setHgrow(Priority.SOMETIMES);
+	        environmentGrid.getColumnConstraints().add(colConstraints);
+	    }
+
+	    for (int i = 0 ; i < GRID_SIZE ; i++) {
+	        RowConstraints rowConstraints = new RowConstraints();
+	        rowConstraints.setVgrow(Priority.SOMETIMES);
+	        environmentGrid.getRowConstraints().add(rowConstraints);
+	    }
+	    
+		for (int i = 0; i<GRID_SIZE; i++) {
+			for (int j = 0; j<GRID_SIZE; j++) {
+				environmentGrid.add(ScreenControl.getCurrentLevel().getEnvironmentTile(i, j).getView(), i, j);
+			}
 		}
+	}
+	
+	private void buildUnitGrid() {
+		unitGrid = new GridPane();
+		// Not sure what these do right now -_-
+		// https://stackoverflow.com/questions/31095954/how-to-get-gridpane-row-and-column-ids-on-mouse-entered-in-each-cell-of-grid-in
+		for (int i = 0 ; i < GRID_SIZE ; i++) {
+	        ColumnConstraints colConstraints = new ColumnConstraints();
+	        colConstraints.setHgrow(Priority.SOMETIMES);
+	        unitGrid.getColumnConstraints().add(colConstraints);
+	    }
+
+	    for (int i = 0 ; i < GRID_SIZE ; i++) {
+	        RowConstraints rowConstraints = new RowConstraints();
+	        rowConstraints.setVgrow(Priority.SOMETIMES);
+	        unitGrid.getRowConstraints().add(rowConstraints);
+	    }
+	    
+		for (int i = 0; i<GRID_SIZE; i++) {
+			for (int j = 0; j<GRID_SIZE; j++) {
+				unitGrid.add(ScreenControl.getCurrentLevel().getUnitTile(i, j).getView(), i, j);
+			}
+		}
+		
 	}
 }
