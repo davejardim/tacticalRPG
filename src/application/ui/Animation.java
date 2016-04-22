ffed3e1f1dfbdb9da9caab0b8551550a28fdb7d2
Christopher Oldham <coldham10@gmail.com>
2016-04-21 23:39:30 -0400

    Beginning animatin

--- src/application/ui/Animation.java
+++ src/application/ui/Animation.java
@@ -2,6 +2,7 @@ package application.ui;
 
 import java.util.ArrayList;
 
+import application.model.game.Game;
 import application.model.unit.Unit;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
@@ -10,16 +11,22 @@ import javafx.util.Duration;
 
 public class Animation {
 
-	public static ArrayList<int[]> getPathCoordinates(int fromX, int fromY, int toX, int toY)
+	public static ArrayList<int[]> getPathCoordinates(Unit unit, int fromX, int fromY, int toX, int toY)
+	//Using A* algorithm
 	{
-		
+		boolean[][] possibleNodes = Game.getValidMoves(fromX, fromY, unit.getMaxMove());
+		ArrayList<int[]> visitedNodes = new ArrayList<>();
+		ArrayList<int[]> needEvaluation = new ArrayList<>();
+		needEvaluation.add(new int[] {0 , fromX , toX} );
 		
 		return null;
 	}
 		
 	
-	public static void moveUnit(Unit unit, int fromX, int fromY, int toX, int toY)
+	public static void moveUnit(Unit unit, int toX, int toY)
 	{
+		int fromX = unit.getXCord();
+		int fromY = unit.getYCord();
 		SequentialTransition sqTrans = new SequentialTransition();
 		ImageView imV = new ImageView(unit.getImage());
 		double distanceTravelled = Math.abs(toX + toY-fromX-fromY);
